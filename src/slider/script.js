ca6baf8684c8eb2df35b190e31345da68a4812d6
AlexViy <alexviy@gmail.com>
2018-01-28 17:10:45 +0200

    Started refactoring

--- src/slider/script.js
+++ src/slider/script.js
@@ -2,36 +2,51 @@ let arrLeft = document.querySelector(".arrow-left");
 let arrRight = document.querySelector(".arrow-right");
 let slider = document.querySelector(".slider");
 let pos = 0;
-let increment = 0;
+let currentSlide = 0;
+let percents = 100;
 let number = document.querySelector(".slides-number")
+let slidesAmount = document.querySelectorAll(".slide").length
 
-arrRight.addEventListener("click", slide, false);
-arrLeft.addEventListener("click", slide, false);
+arrRight.addEventListener("click", whichOne, false);
+arrLeft.addEventListener("click", whichOne, false);
 
-function slide(e) {
+function whichOne(e) {
   if (e.target.classList.contains("arrow-right")) {
-    increment += 100;
-    number.innerHtml=increment;
+    moveLeft();
+  } else {
+    // moveRight();
+    console.log('Left click')
+  }
+}
+
+function moveLeft(){
+  if(currentSlide < slidesAmount) {
     let anim = setInterval(moveIt, 8);
+    currentSlide--;
+
     function moveIt() {
-      if (pos === -increment) {
-        clearInterval(anim);
-      } else {
+      if (pos !== currentSlide*percents) {
+        number.innerHTML = currentSlide +1;
         pos--;
         slider.style.marginLeft = pos + "%";
-      }
-    }
-  } else {
-    increment -= 100;
-    console.log(increment)
-    let anim = setInterval(moveIt, 8);
-    function moveIt() {
-      if (pos === -increment) {
-        clearInterval(anim);
       } else {
-        pos++;
-        slider.style.marginLeft = pos + "%";
+        clearInterval(anim);
       }
     }
   }
+
 }
+
+// function moveRight() {
+//   increment -= percents;
+//   let anim = setInterval(moveIt, 8);
+//   function moveIt() {
+//     if (pos < increment*percents) {
+//       number.innerHTML = increment +1;
+//       pos++;
+//       slider.style.marginLeft = pos + "%";
+//     } else {
+//       clearInterval(anim);
+//     }
+//   }
+// }
