84c6a2491ddad3d320f5a3ee033d3d28506ef75e
AlexViy <alexviy@gmail.com>
2018-01-28 14:18:13 +0200

    Replaced slider logic

---
+++ src/slider/script.js
@@ -0,0 +1,37 @@
+let arrLeft = document.querySelector(".arrow-left");
+let arrRight = document.querySelector(".arrow-right");
+let slider = document.querySelector(".slider");
+let pos = 0;
+let increment = 0;
+let number = document.querySelector(".slides-number")
+
+arrRight.addEventListener("click", slide, false);
+arrLeft.addEventListener("click", slide, false);
+
+function slide(e) {
+  if (e.target.classList.contains("arrow-right")) {
+    increment += 100;
+    number.innerHtml=increment;
+    let anim = setInterval(moveIt, 8);
+    function moveIt() {
+      if (pos === -increment) {
+        clearInterval(anim);
+      } else {
+        pos--;
+        slider.style.marginLeft = pos + "%";
+      }
+    }
+  } else {
+    increment -= 100;
+    console.log(increment)
+    let anim = setInterval(moveIt, 8);
+    function moveIt() {
+      if (pos === -increment) {
+        clearInterval(anim);
+      } else {
+        pos++;
+        slider.style.marginLeft = pos + "%";
+      }
+    }
+  }
+}
