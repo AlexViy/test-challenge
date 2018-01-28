ddb74c477d6983969d433d9a34ba71c66ddbfb71
alexviy <alexviy@gmail.com>
2018-01-28 13:59:30 +0200

    Added menu button behaviour

--- src/components/menubtn/script.js
+++ src/components/menubtn/script.js
@@ -1,1 +1,14 @@
-let menuBtn = document.querySelector('.menu-btn');
+/* Menu drawer button behaviour */
+
+(()=>{
+
+  let menuBtn = document.querySelector('.menu-btn');
+
+  menuBtn.addEventListener('click', activate);
+
+  function activate(){
+    menuBtn.classList.toggle('active');
+  }
+})();
+
+/* End Of Menu drawer button behaviour */
